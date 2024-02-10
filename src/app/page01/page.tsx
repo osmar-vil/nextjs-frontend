90c588eb6f14f689561d7f17e523d47763bfb873
Osmar L. Villaba <osmarvillalbagonzalez@gmail.com>
2024-02-10 10:45:17 -0300

    FEAT: add page01 as example

---
+++ src/app/page01/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function page() {
+  return (
+    <div>
+      <h1>Hello word</h1>
+      <Button variant="contained" color="primary">Hello word Button</Button>
+    </div>
+  );
+}
+
+export default page;
